83053441a4376a8bf23c89c41229483e987a1fe4
BiLosxD <billyrosssta.maria@gmail.com>
2022-02-09 23:39:51 +0800

    initial setup: add css obfuscation: add root.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import root from '../styles/root.css'
+
+import Layout from '../components/globals/layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
+	)
 }
 
 export default MyApp
